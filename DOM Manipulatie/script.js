ebcb3e043a753461a59f7559cf06534b5b8bc6d9
Alexanderdejongh <69797963+Alexanderdejongh@users.noreply.github.com>
2020-09-18 13:01:31 +0000

    Add files via upload

---
+++ DOM Manipulatie/script.js
@@ -0,0 +1,14 @@
+//  ---- DEEL 1 ----
+// _____________________________________________________
+
+// 1. Maak een addEventlistner aan
+const selectFromButtons = document.querySelectorAll(".big-five-button");
+// const listenToButton = collectFromButton.addEventlistner("click", function);
+console.log(selectFromButtons);
+// 2. Maak een functie die gecalled wordt op het moment dat er op 1 an de buttons wordt geklikt. Check dit met een console.log.
+
+// 3. Zorg dat het click event wordt doorgegeven aan deze functie.
+// 4. Zorg dat je toegang hebt tot de waarde (value) van de button waar op is geklikt in deze functie.
+// 5. Creëer een nieuw list-item met JavaScript met de bovenstaande waarde (namelijk het dier waar je op hebt geklikt.
+// 6. Selecteer het parent-element waar je dit nieuwe list-item aan vast wilt maken (de spotted animals list).
+// 7. Maak het nieuwe list-item vast aan het parent-element.
